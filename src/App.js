a9e519a59c046ce0bde931cfabbadea4e8ff6f99
YamataHermoine <shijuzheng1997@126.com>
2023-12-14 22:45:53 -0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import ContentTab from './components/tabs';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/video-react/dist/video-react.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+  const BASE_URL = 'https://api.twdl.online/api/v1'
+  // const BASE_URL = "https://localhost:443/api/v1";
+  useEffect(() => {
+    const initialize = async() => {
+      try{
+          const response = await fetch(BASE_URL + '', {
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        });
+
+        if(!response.ok){
+          console.error('Initialization failed');
+        }
+
+        const data = await response.json();
+        console.log('Initialization successful', data);
+      } catch(error){
+        console.error("Initialization error", error);
+      }
+    };
+    initialize();
+  }, []);
+  return(
+    <div style={{ display: 'block' }}>
+      <ContentTab />
     </div>
-  );
+  )
 }
 
 export default App;
